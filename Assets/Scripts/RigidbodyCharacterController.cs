38575f39becba2fd924099b30777f5f88f424202
Kobakat <54965702+Kobakat@users.noreply.github.com>
2020-09-20 17:25:10 -0500

    Implements physics materials and a demo

--- Assets/Scripts/RigidbodyCharacterController.cs
+++ Assets/Scripts/RigidbodyCharacterController.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class RigidbodyCharacterController : MonoBehaviour
 {
     Rigidbody rb;
-    
-    Vector2 input;
+    Collider col;
 
+    [SerializeField]
+    PhysicMaterial stopPhysicMat;
+    [SerializeField]
+    PhysicMaterial movePhysicMat;
+
+    Vector2 input;
+    
     [SerializeField]
     float acceleration = 10;
     [SerializeField]
@@ -16,6 +22,7 @@ public class RigidbodyCharacterController : MonoBehaviour
     void Start()
     {
         rb = this.GetComponent<Rigidbody>();
+        col = this.GetComponent<Collider>();
     }
     void Update()
     {
@@ -26,11 +33,15 @@ public class RigidbodyCharacterController : MonoBehaviour
     void FixedUpdate()
     {
         Vector3 inputDir = new Vector3(input.x, 0, input.y);
-        input = input.normalized;
+        inputDir = inputDir.normalized;
+
+        col.material = inputDir.sqrMagnitude > 0 ? movePhysicMat : stopPhysicMat;
 
         if(rb.velocity.sqrMagnitude < maxSpeed) 
-            rb.AddForce(inputDir * acceleration);
+            rb.AddForce(inputDir * acceleration, ForceMode.Acceleration);
 
         rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
     }
+
+    
 }
