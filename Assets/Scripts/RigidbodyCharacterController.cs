79244f82505c796528e2e1d83ed7303bebe74d43
Kobakat <54965702+Kobakat@users.noreply.github.com>
2020-09-20 16:31:13 -0500

    Adds force based movement script to player

---
+++ Assets/Scripts/RigidbodyCharacterController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodyCharacterController : MonoBehaviour
+{
+    Rigidbody rb;
+    
+    Vector2 input;
+
+    [SerializeField]
+    float acceleration = 10;
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");       
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 inputDir = new Vector3(input.x, 0, input.y);
+        input.Normalize();
+
+        rb.AddForce(inputDir * acceleration);
+    }
+}
