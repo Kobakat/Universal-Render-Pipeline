164078bda62911f97f1458989667728dac332824
Lienau <380790@colum.edu>
2020-09-30 18:46:12 -0600

    Adds camera relative input

--- Assets/Scripts/RigidbodyCharacterController.cs
+++ Assets/Scripts/RigidbodyCharacterController.cs
@@ -35,10 +35,17 @@ public class RigidbodyCharacterController : MonoBehaviour
         Vector3 inputDir = new Vector3(input.x, 0, input.y);
         inputDir = inputDir.normalized;
 
+        Vector3 camForward = Camera.main.transform.forward;
+        camForward.y = 0;
+
+        Quaternion camLocalRotation = Quaternion.LookRotation(camForward);
+        Vector3 camRelativeInputDir = camLocalRotation * inputDir;
+        
+
         col.material = inputDir.sqrMagnitude > 0 ? movePhysicMat : stopPhysicMat;
 
         if(rb.velocity.sqrMagnitude < maxSpeed) 
-            rb.AddForce(inputDir * acceleration, ForceMode.Acceleration);
+            rb.AddForce(camRelativeInputDir * acceleration, ForceMode.Acceleration);
 
         rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
     }
