dd92b8cd3a1d558c3f599b3d9cfb0fddd94cf679
Kobakat <54965702+Kobakat@users.noreply.github.com>
2020-10-08 14:27:04 -0500

    Adds contextual message controller

---
+++ Assets/Scripts/ContextualMessageController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ContextualMessageController : MonoBehaviour
+{
+    CanvasGroup canvasGroup = null;
+    TMP_Text messageText = null;
+    void Start()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+        messageText = GetComponent<TMP_Text>();
+
+        canvasGroup.alpha = 0;
+    }
+
+    void ShowMessage(string message, float duration)
+    {
+        messageText.text = message;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
